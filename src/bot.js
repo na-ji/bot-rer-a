1531121dd4c93acf2de9022a375b8be9f4159851
Naji Astier <naji.demolitionman@gmail.com>
2016-12-08 16:17:44 +0100

    Initial commit

---
+++ src/bot.js
@@ -0,0 +1,49 @@
+// @flow
+'use strict';
+
+let Wit = require('node-wit').Wit;
+let interactive = require('node-wit').interactive;
+let _ = require('lodash');
+
+const accessToken = (() => {
+  if (process.argv.length !== 3) {
+    console.log('usage: node src/bot.js <wit-access-token>');
+    process.exit(1);
+  }
+  return process.argv[2];
+})();
+
+const firstEntityValue = (entities, entity) => {
+  const val = entities && entities[entity] &&
+    Array.isArray(entities[entity]) &&
+    entities[entity].length > 0 &&
+    entities[entity][0].value
+  ;
+  if (!val) {
+    return null;
+  }
+  return typeof val === 'object' ? val.value : val;
+};
+
+const actions = {
+  send (request, response) {
+    const {sessionId, context, entities} = request;
+    const {text, quickreplies} = response;
+    console.log('sending...', JSON.stringify(response));
+  },
+  getSchedule ({context, entities}) {
+    var origin : null = firstEntityValue(entities, 'origin');
+    var direction = firstEntityValue(entities, 'direction');
+    if (origin && direction) {
+      context.schedule = '{schedule}';
+      delete context.missingDirection;
+    } else {
+      context.missingDirection = true;
+      delete context.schedule;
+    }
+    return context;
+  }
+};
+
+const client = new Wit({accessToken, actions});
+interactive(client);
